c2cf38902ecd2b7993b98f396d3853b95891c2bc
huy <sliverzack1@gmail.com>
2026-01-01 16:20:59 +0700

    feat: Add field Role for User table

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Primitives;
+﻿using Domain.Enums;
+using Domain.Primitives;
 
 namespace Domain.Entities
 {
@@ -8,17 +9,19 @@ namespace Domain.Entities
         public string Password { get; private set; } = string.Empty;
         public string Email { get; private set; } = string.Empty;
         public string Phone { get; private set; } = string.Empty;
+        public UserRole Role { get; private set; }
         public DateTime CreatedAt { get; private set; }
         private readonly List<Task> _tasks = new();
         public IReadOnlyCollection<Task> Tasks => _tasks.AsReadOnly();
 
-        public User(Guid id, string username, string password, string email, string phone, DateTime createdAt)
+        public User(Guid id, string username, string password, string email, string phone, UserRole role, DateTime createdAt)
             : base(id)
         {
             Username = username;
             Password = password;
             Email = email;
             Phone = phone;
+            Role = role;
             CreatedAt = createdAt;
         }
 
